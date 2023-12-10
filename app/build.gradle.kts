c8e0d9cad7af1214b6d6e6673c203368b9d9eba9
Murad Najafli <muradnecefli05@gmail.com>
2023-12-11 03:04:02 +0400

    - Implemented Dagger-Hilt along with its modules (ViewModelModule, RepositoryModule, DatabaseModule) - Added UiText for StringResources usage in ViewModel - Created interfaces for Repositories and UseCases.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
